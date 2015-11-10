ef4c1d87ba7e5bbde0b0fcb94247098cb073ded1
Ryan Heard <ryanh@mycomicshop.com>
2015-11-10 16:20:01 -0600

    cnverts file to RGB and converts morse

--- morseRead.py
+++ morseRead.py
@@ -1,49 +1,68 @@
+
+import sys, os
 from PIL import Image
-import os
+
+def get_refined_image():
+    #This is going to take the file and turn it into a workable picture.
+    im = Image.open(sys.argv[1])
+    im.load()
+    rbg_im = im.convert('RGB')
+
+    return rbg_im
 
 
-
-def get_image_info():
-    command =  'cd %HOMEPATH%\Pictures && cd'
-    findFolder = os.popen(command).read().strip()
-    fileFound = findFolder +'/'+'TEST.jpg' #have this be some kind of raw
-
-    im = Image.open(fileFound)
-
-
-    print(im.mode)
-    print(os.path.isfile(fileFound) )
-
-    return im
-
-
-def get_positions(im_info):
+def get_morse(im_info):
     last_found = 0
     current = 0
-    ascii_chars = []
+    ascii_chars = ""
     pix = im_info.load()
 
-    '''
-    for i in range(0, im_info.size[0]):
-        for j in range(0,im_info.size[1]):
-            if pix[i,j] == (255,255,255):
-                current = (i*100)+j
-                ascii_chars.append(chr(current - last_found)) #append char equiv.
-                print(current - last_found)
+    for i in range(0, im_info.size[1]):
+        for j in range(0, im_info.size[0]):
+            if pix[j,i] != (0,0,0):
+                current = j+(i*100)
+                ascii_chars += chr(current - last_found)
                 last_found = current
-    '''
 
+    ascii_chars = ascii_chars.strip().split(' ')
+    print(ascii_chars)
     return ascii_chars
 
-info = get_image_info()
-print(info.size)
-refined_output = get_positions(info)
+def moorse_ciper(raw,invert):
+        code = {'A': '.-','B': '-...','C': '-.-.',
+        'D': '-..','E': '.','F': '..-.',
+        'G': '--.','H': '....','I': '..',
+        'J': '.---','K': '-.-','L': '.-..',
+        'M': '--','N': '-.','O': '---',
+        'P': '.--.','Q': '--.-','R': '.-.',
+     	'S': '...','T': '-','U': '..-',
+        'V': '...-','W': '.--','X': '-..-',
+        'Y': '-.--','Z': '--..',
+        '0': '-----','1': '.----','2': '..---',
+        '3': '...--','4': '....-','5': '.....',
+        '6': '-....','7': '--...',  '8': '---..',
+        '9': '----.'
+        }
+        translated = ""
 
-#print(refined_output)
+        if invert:
+            code = dict([[v,k] for k,v in code.items()])
 
+        for elm in raw:
+            try:
+                translated += code[elm]
+            except:
+                translated += ' '
 
+        return translated
 
+def main():
+    im = get_refined_image()
+    morse_list = get_morse(im)
 
+    print(moorse_ciper(morse_list, True))
 
-#im = Image.open("dead_parrot.jpg") #Can be many different formats.
-#pix = im.load(
+    test = ".- / - . ... -".split(' ')
+    print('Test: '+moorse_ciper(test,True))
+
+main()
